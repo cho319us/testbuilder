6ac1b583e60cc471f5a72c5af3fcf659a2293347
cho319 <cho319us@gmail.com>
2020-04-24 12:24:23 -0700

    Successfully pass the automated test with 'Visa', 'MasterCard', 'American Express', or 'Diner's Club'

--- detectNetwork.js
+++ detectNetwork.js
@@ -1,18 +1,46 @@
-// Given a credit card number, this function should return a string with the 
+// Given a credit card number, this function should return a string with the
 // name of a network, like 'MasterCard' or 'American Express'
 // Example: detectNetwork('343456789012345') should return 'American Express'
 
-// How can you tell one card network from another? Easy! 
+// How can you tell one card network from another? Easy!
 // There are two indicators:
 //   1. The first few numbers (called the prefix)
 //   2. The number of digits in the number (called the length)
 
 var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
-  // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
-  // The American Express network always starts with a 34 or 37 and is 15 digits long
 
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
+  var cardData = [{ // The American Express network always starts with a 34 or 37 and is 15 digits long
+    network: 'American Express',
+    prefixes: ['34', '37'],
+    lengths: [15]
+  },{ // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
+    network: 'Diner\'s Club',
+    prefixes: ['38', '39'],
+    lengths: [14]
+  },{ // Visa always has a prefix of 4 and a length of 13, 16, or 19.
+    network: 'Visa',
+    prefixes: ['4'],
+    lengths: [13, 16, 19]
+  },{ // MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
+    network: 'MasterCard',
+    prefixes: ['51', '52', '53', '54', '55'],
+    lengths: [16]
+  }];
+
+  //iterate the cardData array
+  for(let i=0; i<cardData.length; i++){
+    //check if the length of the cardNumber are includes in the "lengths" array
+    if(cardData[i].lengths.includes(cardNumber.length)){
+      //iterate over the "prefixes" array in the cardData[i]
+      for(let j=0; j<cardData[i].prefixes.length; j++){
+        //check if the prefixes of the cardNumber are match with string in the "prefixes" array
+        if(cardNumber.substring(0, cardData[i].prefixes[j].length) === cardData[i].prefixes[j]){
+          return cardData[i].network;
+        }
+      }
+    }
+  }
 };
 
 
